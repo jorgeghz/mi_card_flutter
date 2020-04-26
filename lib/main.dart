d2cc5f6041166a122700bc52a0333175a51002b8
Jorge García <jorge.garcia@flex.com>
2020-04-26 18:51:08 -0500

    mi card

--- lib/main.dart
+++ lib/main.dart
@@ -10,58 +10,74 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Row(
-            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.fromLTRB(0, 20, 0, 20),
-                height: double.infinity,
-                width: 100,
+          backgroundColor: Colors.blueGrey,
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
 
-                color: Colors.red,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('images/yo.jpg'),
+                ),
+                Text('Jorge García',
+                    style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Pacifico')),
+                Text('Flutter Developer',
+                    style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        color: Colors.teal[100],
+                        fontSize: 20,
+                        letterSpacing: 2,
+                        fontWeight: FontWeight.bold)
+                ),
+                SizedBox(
+                  height: 10,
+                  width: 200,
+                  child: Divider(
+                    color: Colors.teal.shade100
+                  )
+                ),
+                Card(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        size: 35,
+                        color: Colors.teal,
+                      ),
+                      title: Text('+52 3311857581',
+                          style: TextStyle(
+                              color: Colors.teal.shade900,
+                              fontSize: 20,
+                              fontFamily: 'Source Sans Pro')),
+                    )
+                ),
+                Container(),
+                Card(
 
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: 100,
-                    height:100,
-                    color: Colors.yellow,
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        size: 35,
+                        color: Colors.teal,
+                      ),
+                      title: Text('jorgeghz@gmail.com',
+                          style: TextStyle(
+                              color: Colors.teal.shade900,
+                              fontSize: 20,
+                              fontFamily: 'Source Sans Pro')),
 
-
-                  ),
-                  Container(
-                    width: 100,
-                    height:100,
-                    color: Colors.lightGreen,
-
-
-                  ),
-
-                ],
-
-              ),
-
-              Container(
-                margin: EdgeInsets.fromLTRB(0, 20, 0, 20),
-                width:100,
-                height: double.infinity,
-                color: Colors.blue,
-
-
-              )
-
-            ],
-          ),
-        )
+                    )
+                )
+              ],
+            ),
+          )
       ),
     );
   }
 }
-
-
