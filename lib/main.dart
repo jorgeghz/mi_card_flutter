ae29b4d0585c19914e20f02f8bb20f72a220eea0
Jorge García <jorge.garcia@flex.com>
2020-04-26 16:25:39 -0500

    layout challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,65 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                height: double.infinity,
+                width: 100,
+
+                color: Colors.red,
+                child: Text('Hello'),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height:100,
+                    color: Colors.yellow,
+                    child: Text('Container 2'),
+
+                  ),
+                  Container(
+                    width: 100,
+                    height:100,
+                    color: Colors.lightGreen,
+                    child: Text('Container 2-1'),
+
+                  ),
+
+                ],
+
+              ),
+
+              Container(
+                width:100,
+                height: double.infinity,
+                color: Colors.blue,
+                child: Text('Container 3'),
+
+              )
+
+            ],
+          ),
+        )
       ),
-    ),
-  );
+    );
+  }
 }
+
+
